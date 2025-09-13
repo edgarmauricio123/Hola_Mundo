efe3168fbb2d60fe8749fa119e0a7455c4150033
edgarmauricio123 <22620066@tlaxiaco.tecnm.mx>
2025-09-13 13:30:53 -0600

    14.Creacion de ListaAlumnos y de AppBar

--- src/pages/Lista-Alumnos/ListaAlumnos.tsx
+++ src/pages/Lista-Alumnos/ListaAlumnos.tsx
@@ -1,15 +1,26 @@
-import { Ionicons } from '@expo/vector-icons'; import React from 'react';
-import {  StatusBar as RNStatusBar,SafeAreaView, Platform, StatusBar, StyleSheet, View, Text } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import React from 'react';
+import { StatusBar as RNStatusBar, SafeAreaView, Platform, StatusBar, StyleSheet, View, Text, SafeAreaViewBase } from 'react-native';
 
 
-export default function ListaAlumnos(){
+export default function ListaAlumnos() {
     return (
-        <View style={styles.main}>
-            <Text>ListaAlumnos</Text>
- <StatusBar />
-        </View>
+        <SafeAreaView style={styles.main}>
+            {/* INICIO DE APPBAR */}
+            <View style={styles.appBar}>
+                <Ionicons name='arrow-back' size={20} color={'#000'} />
+                <Text style={styles.appBarTitle}>Lista de Usuario</Text>
+                <View style={{ width: 20 }} >
 
-    );
+                </View>
+
+
+            </View>
+
+
+            {/* CIERRE DE APPBAR */}
+        </SafeAreaView>
+    )
 }
 
 const styles = StyleSheet.create({
@@ -17,7 +28,23 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#adeaf9ff',
         paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 44,
-        padding: 36,
+        // padding: 16,
+    },
+    appBar: {
+        height: 56,
+        width: '100%',
+        backgroundColor: '#fac7faff',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        paddingHorizontal: 16,
+    },
+    appBarTitle: {
+        fontWeight: 'bold',
+        fontSize: 20,
     },
 });
+
+
+
 
