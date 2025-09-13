4afe83ea8ca47fea927234b3d1fd552168632f1a
edgarmauricio123 <22620066@tlaxiaco.tecnm.mx>
2025-09-12 21:35:44 -0600

    8.Configuraciones del textInput

--- src/pages/Formulario.tsx
+++ src/pages/Formulario.tsx
@@ -5,20 +5,39 @@ import { TextInput } from 'react-native';
 
 
 export default function Formulario() {
-  return (
-      <SafeAreaView style={styles.main}>
-        <Text>Nombre</Text>
-        <TextInput placeholder='Escribe tu nombre'/>
-      </SafeAreaView>
-  );
+    return (
+        <SafeAreaView style={styles.main}>
+            <Text style={styles.label}>Nombre</Text>
+            <TextInput style={styles.input} placeholder='Escribe tu nombre' />
+            <Text style={styles.label}>Descripción</Text>
+            <TextInput multiline={true} numberOfLines={4} style={[styles.input, styles.textarea]} placeholder='Describe personalidad' />
+        </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
-  main: {
-    flex: 1,
-    backgroundColor: '#f4f4f4ff',
-    padding: Platform.OS === 'android' ? StatusBar.currentHeight : 44,
-  },
-  
+    main: {
+        flex: 1,
+        backgroundColor: '#f4f4f4ff',
+        padding: Platform.OS === 'android' ? StatusBar.currentHeight : 44,
+    },
+    label: {
+        fontWeight: 'bold',
+        marginTop: 20,
+        marginBottom: 5,
+
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: '#000000ff',
+        borderRadius: 10,
+        backgroundColor: '#fff',
+
+    },
+    textarea: {
+        height: 100,
+        textAlignVertical: 'top',
+
+    },
 
 });
